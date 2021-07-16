3f46026eb8700ab7a25d719e6bae6fff6ca757a2
skyfury2651 <skyfury2651@gmail.com>
2021-07-16 20:40:48 +0700

    add serizale

--- src/main/java/com/example/springservice/entity/Product.java
+++ src/main/java/com/example/springservice/entity/Product.java
@@ -9,13 +9,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Product {
+public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int Id;
